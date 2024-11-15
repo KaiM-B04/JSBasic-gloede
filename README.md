6a4818c608acd4a78356c7ea56474a8b5a486296
KaiM-B04 <git@stackblitz.com>
2024-11-15 15:00:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSBasic-gloede
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/KaiM-B04/JSBasic-gloede)
---
+++ script.js
@@ -0,0 +1,61 @@
+// variabler:
+
+const lives = 5;
+
+let currentLives = lives + 1;
+currentLives ++
+// litenBokstavICamelCase
+
+// console.log(lives);
+// console.log(currentLives);
+
+// primitive datatyper: string, numbers, boolean
+let string = '4 5 6 7';
+let newString = string + 1;
+
+// console.log(string);
+// console.log(newString);
+
+let calculation = 4 + 6;
+// console.log(calculation);
+
+let boolean = true;
+// console.log(boolean);
+
+// ikke primitive datatyper: function, array, object
+let myArray = [
+  "mandag", "tirsdag", "onsdag"
+]
+
+// Assingment operators
+
+let x = 10;
+
+x += 4;
+x -= 5;
+x *= 2;
+
+console.log(x);
+
+//Comparison operators
+
+// let a = 10;
+// let b ="10";
+// let c = 5;
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != c);
+// console.log(a > c);
+// console.log(a <= b);
+
+
+// Logical operators && || !
+
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a)
+
